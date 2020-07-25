7b8c891ccbe77a5db76877a7a00a618e7f6231cb
mahede <mahede.object@gmail.com>
2020-07-25 22:48:18 +0600

    compile SASS

--- gulpfile.js
+++ gulpfile.js
@@ -10,33 +10,33 @@ gulp.task('sass', function(){
     .pipe(browserSync.stream());
 });
 
-// Move JS Files to SRC
-gulp.task('js', function(){
-  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/popper.min.js', 'node_modules/fixed-nav/navbar-fixed.js'])
-    .pipe(gulp.dest("src/js"))
-    .pipe(browserSync.stream());
-});
+// // Move JS Files to SRC
+// gulp.task('js', function(){
+//   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/popper.min.js', 'node_modules/fixed-nav/navbar-fixed.js'])
+//     .pipe(gulp.dest("src/js"))
+//     .pipe(browserSync.stream());
+// });
 
-// Watch SASS & Serve
-gulp.task('serve', ['sass'], function(){
-  browserSync.init({
-    server: "./src"
-  });
+// // Watch SASS & Serve
+// gulp.task('serve', ['sass'], function(){
+//   browserSync.init({
+//     server: "./src"
+//   });
 
-  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-  gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+//   gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+//   gulp.watch("src/*.html").on('change', browserSync.reload);
+// });
 
-// Move Font Awesome Fonts folder to src
-gulp.task('fonts', function(){
-  return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest("src/fonts"));
-});
+// // Move Font Awesome Fonts folder to src
+// gulp.task('fonts', function(){
+//   return gulp.src('node_modules/font-awesome/fonts/*')
+//     .pipe(gulp.dest("src/fonts"));
+// });
 
-// Move font awesome css file
-gulp.task('fa', function(){
-  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest("src/css"));
-});
+// // Move font awesome css file
+// gulp.task('fa', function(){
+//   return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+//     .pipe(gulp.dest("src/css"));
+// });
 
-gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
+// gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
